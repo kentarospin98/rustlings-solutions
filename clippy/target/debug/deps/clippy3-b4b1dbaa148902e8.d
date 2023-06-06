efbaedc57685f4da0958b51c613599d0db3cd33f
Shanti X <shanti@lokegaonkar.in>
2023-06-06 02:40:30 -0700

    Finished it!

---
+++ clippy/target/debug/deps/clippy3-b4b1dbaa148902e8.d
@@ -0,0 +1,7 @@
+/home/kensp/Documents/rustlings/exercises/clippy/target/debug/deps/clippy3-b4b1dbaa148902e8.rmeta: clippy3.rs
+
+/home/kensp/Documents/rustlings/exercises/clippy/target/debug/deps/clippy3-b4b1dbaa148902e8.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ functions/functions2.rs
@@ -0,0 +1,13 @@
+// functions2.rs
+// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
+
+
+fn call_me(num: i32) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn main() {
+    call_me(3);
+}
